4ed2c24be7b872009af8be8fbee3adba7d27817e
Americo MQ <desarrollo2@pass.com.mx>
2018-12-11 10:12:28 -0600

    first commit

---
+++ ConsultaLeads - copia.php
@@ -0,0 +1,81 @@
+<?php
+    $servername = "localhost";
+    $username = "Desarrollo";
+    $password = "hIm7RAZqYnSjwxD";
+    $dbname = "passcrm540";
+    //$port = 3306;
+    $port = 33307;
+
+    header('Content-type: application/vnd.ms-excel');
+    header("Content-Disposition: attachment; filename=contactos_totales.xls");
+    header("Pragma: no-cache");
+    header("Expires: 0");
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname, $port);
+    mysqli_set_charset($conn,"utf8");
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    
+
+    if($conn == TRUE){
+        $sql = "SELECT lead_no, concat(firstname,lastname) AS nombre,email,company AS compañia FROM vtiger_leaddetails;"; 
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) {
+        // output data of each row
+        $rows = array();
+        while($row = $result->fetch_assoc()) {
+            $rows[] = $row;
+        }
+        //$rows = array_map('utf8_encode_array', $rows);
+        echo json_encode($rows);
+        } else {
+            if ($conn->error) {
+                printf("Error: %s\n", $conn->error);
+            } else {
+                $cero = array("0 results");
+                echo json_encode($cero);
+            }
+        }
+        /*$table = "<table width='50%' border='1' cellpadding='10' cellspacing='0' bordercolor='#666666' id='Exportar_a_Excel2' style='border-collapse:collapse;' >\n
+		          <thead>\n <tr>\n";
+        $coln = ibase_num_fields($gestor_sent);
+        for ($i = 0; $i < $coln; $i++) {
+            $col = ibase_field_info($gestor_sent, $i);
+            $table .= '<th>'.$col['alias'].'</th>';
+        }
+        $table .=	"</tr>\n
+                    </thead>\n";
+
+        $table .= "<tbody>\n";
+	
+	
+        $num_rows = 0;
+        while ($row[$num_rows] = ibase_fetch_assoc($gestor_sent)) {
+            $num_rows++;
+        }
+
+        for($i = 0; $i < $num_rows; $i++) {
+            $table .= "<tr>\n";
+            foreach($row[$i] as $field) {
+                $table .= "<td>" . utf8_encode($field) . "</td>\n";
+            }
+            $table .= "</tr>\n";
+        }
+	
+        $table .= "</tbody>\n".
+                  "</table>";
+
+        echo $table;
+
+        ibase_free_result($gestor_sent);
+        ibase_close($gestor_db); */
+    
+}else{
+    echo"Error al conectar con la BD, comunicate con el Desarrollador!!";
+}
+
+    
+?>
